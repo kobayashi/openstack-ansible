734f2283401531ee98732e5a032093c02331fd23
chiisaihayashi <abok.1k@gmail.com>
2015-12-07 15:39:34 +0900

    Changes to be committed: 	modified:   roles/keystone/tasks/main.yml 	modified:   roles/keystone/templates/admin-openrc.sh 	new file:   roles/keystone/templates/apache2.conf 	modified:   roles/keystone/templates/demo-openrc.sh 	modified:   roles/keystone/templates/keystone.conf 	modified:   roles/keystone/templates/keystone_init 	new file:   roles/keystone/templates/service_init 	new file:   roles/keystone/templates/wsgi-keystone.conf

--- roles/keystone/templates/admin-openrc.sh
+++ roles/keystone/templates/admin-openrc.sh
@@ -1,6 +1,9 @@
 # Verify the Identity Service installation
-export OS_PASSWORD={{ ADMIN_PASS }}
+export OS_PROJECT_DOMAIN_ID=default
+export OS_USER_DOMAIN_ID=default
+export OS_PROJECT_NAME=admin
 export OS_TENANT_NAME=admin
-export OS_AUTH_URL=http://{{ controller_host }}:35357/v2.0
-export OS_USERNAME=ADMIN
-
+export OS_USERNAME=admin
+export OS_PASSWORD={{ ADMIN_PASS }}
+export OS_AUTH_URL=http://{{ controller_host }}:35357/v3
+export OS_IDENTITY_API_VERSION=3
--- roles/keystone/templates/demo-openrc.sh
+++ roles/keystone/templates/demo-openrc.sh
@@ -1,5 +1,8 @@
+export OS_PROJECT_DOMAIN_ID=default
+export OS_USER_DOMAIN_ID=default
+export OS_PROJECT_NAME=demo
+export OS_TENANT_NAME=demo
 export OS_USERNAME=demo
 export OS_PASSWORD={{ DEMO_PASS }}
-export OS_TENANT_NAME=demo
-export OS_AUTH_URL=http://{{ controller_host }}:35357/v2.0
-
+export OS_AUTH_URL=http://{{ controller_host }}:5000/v3
+export OS_IDENTITY_API_VERSION=3
